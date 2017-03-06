3c8c15fd5621d4f072ccbdbe3f5046d0bf9f1e7b
ridiks10 <ridiks100@gmail.com>
2017-03-06 18:24:22 +0200

    point 2

--- src/main.h
+++ src/main.h
@@ -34,7 +34,7 @@ class CNode;
 // Global state
 //
 
-static const int LAST_POW_BLOCK = 50;
+static const int LAST_POW_BLOCK = 80000;
 
 static const unsigned int MAX_BLOCK_SIZE = 1000000;
 static const unsigned int MAX_BLOCK_SIZE_GEN = MAX_BLOCK_SIZE/2;
